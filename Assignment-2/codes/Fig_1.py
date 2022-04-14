f052a971fe776b7bdba64e8ee3d312d7dbd14c4c
K-Vivek-Kumar <102508246+K-Vivek-Kumar@users.noreply.github.com>
2022-04-14 11:45:23 +0530

    Add files via upload

---
+++ Assignment-2/codes/Fig_1.py
@@ -0,0 +1,21 @@
+# importing the important libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+# defining the function
+def f(x):
+    return np.sqrt((2*x) - 3)
+
+# forming the plotting axis values
+xlist = np.linspace(1.5, 100, num=1000)
+ylist = f(xlist)
+
+# plotting f(x)
+plt.figure(num=0, dpi=120)
+plt.plot(xlist,ylist,label="f(x)")
+plt.title("Plotting the function")
+plt.xlabel("Values of x")
+plt.ylabel("Values of f(x)")
+plt.grid()
+plt.legend()
+plt.show()
