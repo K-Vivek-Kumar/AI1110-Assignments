4222c1409457f3419bca769b0509510e8eb2665d
K-Vivek-Kumar <102508246+K-Vivek-Kumar@users.noreply.github.com>
2022-04-27 16:27:14 +0530

    Update Figures.py

--- Assignment-3/codes/Figures.py
+++ Assignment-3/codes/Figures.py
@@ -1,6 +1,7 @@
 # Importing appropriate libraries
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 """
 Constructing a frequency polygon for the data.
@@ -12,13 +13,8 @@ Figure_1
 Fig_1 = plt.figure(1)
 
 # Data given
-x1 = [145, 145, 145, 145, 145
-        ,155 , 155, 155, 155, 155, 155, 155, 155, 155, 155
-    ,165,165,165,165,165,165,165,165,165,165,165,165,165,165,165,165,165,165,165,165
-    ,175,175,175,175,175,175,175,175,175
-    ,185,185,185,185,185,185
-    ,195,195]
-bins = [140,150,160,170,180,190,200]
+val = pd.read_excel("values.xlsx","Sheet1",usecols="A",nrows=52)
+x1 = np.array(val)
 
 # Advancing the plots
 plt.title("Constructing a Histogram of the given data", fontsize=15)
@@ -27,10 +23,9 @@ plt.ylim(0, 21)
 plt.grid(alpha=0.45)
 
 #Plotting the histogram values
-plt.hist(x1,bins,color='blue',edgecolor ='red',alpha=0.7)
+plt.hist(x1,bins=[140,150,160,170,180,190,200],edgecolor='red',alpha=0.7)
 plt.xlabel("Cost of living index", fontsize=15)
 plt.ylabel("Number of weeks", fontsize=15)
-
 
 """
 Constructing a frequency polygon for the data.
@@ -42,8 +37,15 @@ Figure_2
 Fig_2 = plt.figure(2)
 
 # The data given
-x = [135,145,155,165,175,185,195,205,135]
-y = [0,5,10,20,9,6,2,0,0]
+# x-values while plotting the frequency polygon
+val = pd.read_excel("values.xlsx","Sheet1",usecols="B",nrows=9)
+xe = np.array(val)
+x = xe.ravel()
+
+# y-values while plotting the frequency polygon
+val = pd.read_excel("values.xlsx","Sheet1",usecols="C",nrows=9)
+ye = np.array(val)
+y = ye.ravel()
 
 # Limits for the graph parameters
 plt.xlim(125, 215)
